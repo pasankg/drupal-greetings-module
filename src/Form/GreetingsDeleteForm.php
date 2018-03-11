7f1c68726abf65d1639dbdd4abf387d5640f73c2
Pasan Gamage <pasan.gamage@previousnext.com.au>
2018-03-11 13:11:29 +1100

    [#master] Add Forms

---
+++ src/Form/GreetingsDeleteForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\greetings\Form;
+
+use Drupal\Core\Entity\ContentEntityDeleteForm;
+/**
+ * Provides a form for deleting a greetings entity.
+ *
+ * @ingroup greetings
+ */
+class GreetingsDeleteForm extends ContentEntityDeleteForm {
+
+}
---
+++ src/Form/GreetingsForm.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Drupal\greetings\Form;
+
+use Drupal\Core\Entity\ContentEntityForm;
+use Drupal\Core\Language\Language;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Form controller for the greetings entity edit forms.
+ *
+ * @ingroup greetings
+ */
+class GreetingsForm extends ContentEntityForm {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    /* @var $entity \Drupal\greetings\Entity\Greetings */
+    $form = parent::buildForm($form, $form_state);
+    $entity = $this->entity;
+
+    $form['langcode'] = array(
+      '#title' => $this->t('Language'),
+      '#type' => 'language_select',
+      '#default_value' => $entity->getUntranslated()->language()->getId(),
+      '#languages' => Language::STATE_ALL,
+    );
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function save(array $form, FormStateInterface $form_state) {
+    $status = parent::save($form, $form_state);
+
+    $entity = $this->entity;
+    if ($status == SAVED_UPDATED) {
+      drupal_set_message($this->t('The greeting %feed has been updated.', ['%feed' => $entity->toLink()->toString()]));
+    } else {
+      drupal_set_message($this->t('The greeting %feed has been added.', ['%feed' => $entity->toLink()->toString()]));
+    }
+
+    $form_state->setRedirectUrl($this->entity->toUrl('collection'));
+    return $status;
+  }
+}
